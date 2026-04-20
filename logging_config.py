43f50ec70997ebe87ba916b20507d23a68643b27
Vitlaii Tsurka <Fakerovi4i@gmail.com>
2026-04-20 23:17:05 +0300

    Update IDE workspace state

--- logging_config.py
+++ logging_config.py
@@ -24,6 +24,6 @@ def setup_logging(level=logging.INFO):
     logging.getLogger("urllib3").setLevel(logging.WARNING)
     logging.getLogger("telebot").setLevel(logging.WARNING)
     logging.getLogger("requests").setLevel(logging.WARNING)
-    logging.getLogger("flask").setLevel(logging.WARNING)
+    logging.getLogger("werkzeug").setLevel(logging.ERROR)
 
 
