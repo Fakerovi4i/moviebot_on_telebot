c334eac9103b51e6043bfd48867abe28b6b57d07
Vitlaii Tsurka <Fakerovi4i@gmail.com>
2026-04-20 18:23:15 +0300

    Update IDE workspace state

--- main.py
+++ main.py
@@ -3,10 +3,14 @@
 """
 import logging
 from logging_config import setup_logging
+import time
+from telebot.apihelper import ApiTelegramException
 
 from bot import bot
 from database.db_connection import init_db
 from handlers.base import register_handlers
+
+
 
 
 def main():
@@ -17,13 +21,25 @@ def main():
     init_db()
     register_handlers()
 
-    logger.info("🤖 Бот начал работу...")
-
-    bot.infinity_polling(
-        skip_pending=True,
-        timeout=60,
-        long_polling_timeout=60
-    )
+    while True:
+        try:
+            logger.info("🤖 Бот начал работу...")
+            bot.infinity_polling(
+                skip_pending=True,
+                timeout=60,
+                long_polling_timeout=60
+            )
+        except ApiTelegramException as e:
+            if e.result_json.get('error_code') == 409:
+                logger.warning("Обнаружен конфликт экземпляров (409). Перезапуск через 5 секунд...")
+                time.sleep(5)
+                continue
+            else:
+                logger.critical(f"Неизвестная ошибка Telegram API: {e}", exc_info=True)
+                break
+        except Exception as e:
+            logger.critical(f"Критическая ошибка: {e}", exc_info=True)
+            break
 
 
 if __name__ == '__main__':
